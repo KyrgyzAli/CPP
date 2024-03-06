edffc88db6dcaf87782e72cf88f36f0337e67fea
KyrgyzAli <159999059+KyrgyzAli@users.noreply.github.com>
2024-03-07 04:39:56 +0800

    Add files via upload

---
+++ S01 (1б).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    //Создание переменных
+   string name="Ali";
+   string surname="Kuchmenov";
+
+   int number = 24;
+   float number2 =2.4f;
+   bool happy= true;
+
+   //Aрифметические операции
+   number=number / 24;
+   number2=number2 - 1.2;
+   name=name + surname;
+   number=number * 5;
+   cout<<number <<::endl;
+   cout<<number2 <<::endl;
+   cout<<name <<::endl;
+   cout<<number <<::endl;
+
+
+    return 0;
+}
