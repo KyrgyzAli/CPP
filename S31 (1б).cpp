43300088a007a0846966a6ed52edb80313d41c84
KyrgyzAli <159999059+KyrgyzAli@users.noreply.github.com>
2024-03-07 04:58:30 +0800

    Add files via upload

---
+++ S31 (1б).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+    setlocale(0,"Russian");
+    int n;
+    string numbers[10]={"НОЛЬ","Один","Два","Три","Четыре","Пять","Шесть","Семь","Восемь","Девять"};
+    cout <<"Введите число от 0 до 9"<<::endl;
+    cin >> n ;
+    cout << numbers[n];
+
+
+    return 0;
+}
