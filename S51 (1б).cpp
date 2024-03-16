3bc2ba525fd4f93811bb6cc816620abb38b8e162
KyrgyzAli <159999059+KyrgyzAli@users.noreply.github.com>
2024-03-16 17:59:20 +0800

    Add files via upload

---
+++ S51 (1б).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int reverse(){
+string s;
+s="Testing";
+for (int i = 6; i > -1; i--) {
+    cout << s[i] ;
+}
+}
+
+int main() {
+    reverse();
+    return 0;
+}
