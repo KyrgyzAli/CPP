6730c7a4d75ed3cb536fa00dbb4d132437d4ac47
KyrgyzAli <159999059+KyrgyzAli@users.noreply.github.com>
2024-03-22 18:26:58 +0800

    Add files via upload

---
+++ S42 (2б).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int sort(){
+    int arr[10]={1, 8, 3, 6, 5, 4, 9, 2, 8, 12};
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10-1-i;j++){
+            if(arr[j]< arr[j+1]){
+            swap(arr[j],arr[j+1]);
+            }
+        }
+
+    }
+    for (int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }
+
+}
+int main(){
+    sort();
+    return 0;
+}
