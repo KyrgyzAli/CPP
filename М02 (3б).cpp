95f5f27b2b715c7e930861f4f9d2de606a20dba4
KyrgyzAli <159999059+KyrgyzAli@users.noreply.github.com>
2024-04-08 11:35:56 +0800

    Update М02 (3б).cpp

--- М02 (3б).cpp
+++ М02 (3б).cpp
@@ -7,7 +7,7 @@ using namespace std;
 int ploshad(int a, int n){
     //Ôîðìóëó ðàñ÷åòà ïëîùàäè ìíîãîóãîëüíèêà ÿ èñïîëüçóþ ïî äëèíå ñòîðîíû
     int s;
-    s=(n*(a*a))/(4 * tan(180/n));
+    s=(n*(a*a))/(4 * tan((180*180/3.14)/n));
     return s;
 
 }
