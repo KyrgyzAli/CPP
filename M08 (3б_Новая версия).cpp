defe98f9be0a9c339dc8343b8cdfae5f3163fd83
KyrgyzAli <159999059+KyrgyzAli@users.noreply.github.com>
2024-04-08 11:34:19 +0800

    Update M08 (3б_Новая версия).cpp

--- M08 (3б_Новая версия).cpp
+++ M08 (3б_Новая версия).cpp
@@ -5,19 +5,20 @@
 using namespace std;
 int get_angle(int a,int b,int c)
 {
-if (a<=0 || b<=0 || c<=0 ){
-    cout << "Error you entered incorrectly!";
-    return 0;
-}
-else {
-    float d,rad;
-    int grad;
-    d=((b*b)+(c*c)-(a*a))/(2*b*c); //ÐÀÑ×ÈÒÀË ÓÃÎË Â ÐÀÄÈÀÍÀÕ
-    rad=acos(d);
-    grad=rad*180/3.14;
-    return grad;
-}
-}
+    if (a<=0 || b<=0 || c<=0 ){
+        cout << "Error you entered incorrectly!";
+        return 0;
+    }
+    else {
+        float d,rad;
+        int grad;
+        d=((b*b)+(c*c)-(a*a))/(2*b*c); 
+        rad=acos(d);
+        grad=rad*180/3.14;
+        return grad;
+    }
+    }
+
 int main(){
     cout<< get_angle(5.,4.,3.);
     return 0;
