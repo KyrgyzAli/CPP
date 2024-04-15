90e0f7a12692bf1ad6ef1dbcef510bcd7da187a7
KyrgyzAli <159999059+KyrgyzAli@users.noreply.github.com>
2024-04-15 21:21:02 +0800

    Update S42 (2б).cpp

--- S42 (2б).cpp
+++ S42 (2б).cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int sort(){
-    int arr[10]={1, 8, 3, 6, 5, 4, 9, 2, 8, 12};
+int sort(int arr[]){
     for (int i=0;i<10;i++){
         for (int j=0;j<10-1-i;j++){
             if(arr[j]< arr[j+1]){
             swap(arr[j],arr[j+1]);
             }
         }
-
     }
     for (int i=0;i<10;i++){
         cout<<arr[i]<<" ";
     }
-
 }
 int main(){
-    sort();
+    int arr[10]={1, 8, 3, 6, 5, 4, 9, 2, 8, 12};
+    sort(arr);
     return 0;
 }
